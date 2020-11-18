292bcd02e84ad1c2a729620285157cbb98aae87c
RoniPolisanov <yyyroni@gmail.com>
2020-11-19 01:17:37 +0200

    Update

--- TaskScheduler/conrollers/Flow.ctrl.js
+++ TaskScheduler/conrollers/Flow.ctrl.js
@@ -30,7 +30,21 @@ app.get('/status', async (req, res) => {
 
 app.post('/add', async (req, res) => {
     try {
+        let body = req.body;
 
+        if (body instanceof Object) {
+            let item = Object.assign({}, body);
+
+            let flowObj = (new Flow(item)).initFlow();
+
+
+            if (!flowObj.isError) {
+                const flowRes = await schedule();
+                return res.status(200).send(flowRes);
+            }
+
+            return res.status(500).send(flowObj);
+        }
     }
     catch (err) {
         console.log(err);
@@ -40,7 +54,18 @@ app.post('/add', async (req, res) => {
 
 const schedule = async () => {
     return new Promise(resolve => {
+        self.userid = JSON.parse(sessionStorage.getItem('userDetails'));
 
+        // Get the user details from database
+        axios.post(`${process.env.HOST_NAME}:${process.env.EXECUTIVE_PORT}/task/schedule`)
+            .then(userResponse => {
+                self.setState({ userDetails: userResponse.data, loading: false });
+                console.log('Menu was loaded successfully');
+                resolve(userResponse.data);
+            })
+            .catch(error => {
+                fetchDataHandleError(error);
+            });
     });
 }
 
